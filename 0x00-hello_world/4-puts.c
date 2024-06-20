9a52ca9a85b33699a97c0a4dc00fbe73e0fac542
VictorEne-Codes <omalevicene@gmail.com>
2024-06-20 15:14:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	puts(stderr, "Error"):
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
