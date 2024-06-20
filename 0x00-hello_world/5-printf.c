96bd48dae1c4bd9e1cd77f3bb8be4f93250a37c8
VictorEne-Codes <omalevicene@gmail.com>
2024-06-20 15:24:42 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,11 @@
  *
  * Description: This function prints out texts
  *
+ * Return: 0
+ *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
