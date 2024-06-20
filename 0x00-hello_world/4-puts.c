fcc562b88ba5b44309ea0496b7af01aa25103bc7
VictorEne-Codes <omalevicene@gmail.com>
2024-06-20 14:52:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-i#include <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n")
-	return (0); 
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
