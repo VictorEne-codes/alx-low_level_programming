fb2fb7fd1b43b7b8c85981a1ceb30a049f525faa
VictorEne-Codes <omalevicene@gmail.com>
2024-06-20 15:04:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: entry point
+ * main - entry point
  *
- *This functuons returns a strung of texts
+ *Description: This functuons returns a strung of texts
  *
  * Return: 0
- * */
+ *
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
