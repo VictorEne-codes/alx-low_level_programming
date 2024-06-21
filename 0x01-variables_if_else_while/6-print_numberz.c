564035ff3f9fe74afab51fd19ef73e290d82728a
VictorEne-Codes <omalevicene@gmail.com>
2024-06-21 14:02:14 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints digit numbers 0-9
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	while (a <= 9)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
