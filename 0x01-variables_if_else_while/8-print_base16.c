f9aa464c7b1e9c7dc9f19c06490f350a5b196749
VictorEne-Codes <omalevicene@gmail.com>
2024-06-21 14:13:41 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print hexadecimal digits in lpwecase
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char A = 1;
+
+	while (A <= 9)
+	{
+		putchar('0' + A);
+		A++;
+	}
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
