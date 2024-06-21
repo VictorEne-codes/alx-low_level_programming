da4b12f18d852017adb366a34e4b33896b10ce03
VictorEne-Codes <omalevicene@gmail.com>
2024-06-21 15:40:19 -0700

    combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,24 @@
 int main(void)
 {
 	int i, j, k;
+
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			for (k = j; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar('0' + i);
-            			putchar('0' + j);
+				putchar('0' + j);
 				putchar('0' + k);
-				putchar(',');
-				putchar(' ');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
