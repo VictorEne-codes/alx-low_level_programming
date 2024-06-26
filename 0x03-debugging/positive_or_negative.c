0824cd100704c8bc7208e181f56d92ebb4d77e76
VictorEne-Codes <omalevicene@gmail.com>
2024-06-26 07:47:32 -0700

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - a funtion to check for positive or negative numbers
+ * @i: input param
+ * Return: always return 0
+ */
+
+
+int positive_or_negative(int i)
+{
+	i = 0;
+	/*srand(time(0));
+	i = rand() - RAND_MAX / 2;*/
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 9)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("empty");
+	}
+	return (0);
+}
