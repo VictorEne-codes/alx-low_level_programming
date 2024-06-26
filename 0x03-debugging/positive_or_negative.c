5c528ca3e2a86b5d3b530bd0db9345bd52d2b07d
VictorEne-Codes <omalevicene@gmail.com>
2024-06-26 08:02:12 -0700

    debugging again

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,9 +11,10 @@
 
 int positive_or_negative(int i)
 {
-	i = 0;
-	/*srand(time(0));
-	i = rand() - RAND_MAX / 2;*/
+	/**
+	 * srand(time(0));
+	 * i = rand() - RAND_MAX / 2;
+	*/
 	if (i > 0)
 	{
 		printf("%d is positive\n", i);
