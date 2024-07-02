c11275e1db261a37c9201fd8a64de881c4cd074d
VictorEne-Codes <omalevicene@gmail.com>
2024-07-01 18:01:54 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints have of the string
+ * @str: input
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	if (length % 2 == 0)
+	{
+		length = length / 2;
+	}
+	else
+	{
+		length = (length - 1) / 2;
+	}
+	for (; str[length] != '\0'; length++)
+	{
+		_putchar(str[length]);
+	}
+	_putchar('\n');
+}
