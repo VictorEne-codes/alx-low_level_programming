de5a98e0357cef1eb3fa9f0f06a08bd96de721c0
VictorEne-Codes <omalevicene@gmail.com>
2024-07-03 05:01:53 -0700

    task 5 dont

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * string_toupper - convert strings to uppercase
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = toupper((unsigned char) str[i]);
+		i++;
+	}
+	return str;
+}
