2faa9737c63faa0cc3ff1d024723c695ebd27b58
VictorEne-Codes <omalevicene@gmail.com>
2024-07-03 04:44:09 -0700

    updated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,9 +12,13 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -n;
+		n = -n;
 	}
-	if (n / 10 != 0)
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository containz tasks for my furst C programming tasks.
