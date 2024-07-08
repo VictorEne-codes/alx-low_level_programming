064cc1126934f3895bb9267a72c3b7bff94cef97
VictorEne-Codes <omalevicene@gmail.com>
2024-07-08 14:13:00 -0700

    task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - set pointer yo char
+ * @s: input
+ * @to: input
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
