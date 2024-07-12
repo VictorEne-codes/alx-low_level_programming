6cbe7d7d761af7a275e1bd9f1560c4a30b7f9237
VictorEne-Codes <omalevicene@gmail.com>
2024-07-12 12:00:17 -0700

    task 3 again

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-	int mul;
+	int i, mul = 1;
 
 	if (argc > 1)
 	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
+		for (i = 1; i < argc; i++)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+		}
 		printf("%d\n", mul);
 	}
 	else
