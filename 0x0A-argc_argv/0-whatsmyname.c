4bc6a4bba4bb2a4d5baedbc22883f11821f01a30
VictorEne-Codes <omalevicene@gmail.com>
2024-07-11 23:25:58 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - ptint nane of file
+ * @argc: input
+ * @argv: input
+ *
+ * Return: int
+ */
+
+int main(int argc, char* argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks for C argc and argv
