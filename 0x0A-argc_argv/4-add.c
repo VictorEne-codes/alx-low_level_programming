bb1a065de8d29a07a0ee9ab2a0ae42d8f6feefa8
VictorEne-Codes <omalevicene@gmail.com>
2024-07-13 14:25:40 -0700

    task 4 done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
  * main - adds posutive  nimbers
  * @argc: input
@@ -11,24 +11,23 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc == 1)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (atoi(argv[i]) < 48 || atoi(argv[i]) > 57)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
-			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
+
+		sum += atoi(argv[i]);
 	}
+
+	printf("%d\n", sum);
+
 	return (0);
 }
