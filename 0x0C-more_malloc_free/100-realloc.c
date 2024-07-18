8c5ff57e70e62153ada154819602c0520b162b58
VictorEne-Codes <omalevicene@gmail.com>
2024-07-18 16:23:43 -0700

    gmtask 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *new_ptr;
 
-	if (new_size == old_size)
+	if (old_size == new_size)
 	{
 		return (ptr);
 	}
@@ -27,7 +27,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		new_ptr = malloc(new_size);
+		return (new_ptr);
 	}
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
