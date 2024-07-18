3861b3a1ea63a35b070f15da5a88fd020e672196
VictorEne-Codes <omalevicene@gmail.com>
2024-07-18 16:10:40 -0700

    gmtask 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - reallocates a mememory block
+ * @old_size: input
+ * @new_size: input
+ * @ptr: input
+ *
+ * Return: void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	void *new_ptr;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	}
+	free(ptr);
+	return (new_ptr);
+}
