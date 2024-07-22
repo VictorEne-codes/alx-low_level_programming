d563bb083c6f1bf021fd89f572a15b09760cc666
VictorEne-Codes <omalevicene@gmail.com>
2024-07-22 15:15:53 -0700

    task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks for C structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - about a dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ *
+ * Description: this struct describes a dog
+ */
+
+struct dog 
+{
+	char * name;
+	float age;
+	char * owner;
+}
+
+#endif
