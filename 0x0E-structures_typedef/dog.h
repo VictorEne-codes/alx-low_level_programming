f7eb1f10e1db8351c215b9204743082ea4365f42
VictorEne-Codes <omalevicene@gmail.com>
2024-07-22 16:14:08 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,16 @@
  * Description: this struct describes a dog
  */
 
+/**
+ * dog_t - typedef for dog_t
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
