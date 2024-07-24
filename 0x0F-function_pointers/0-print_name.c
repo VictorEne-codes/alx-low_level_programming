a8fd8aa8158ed3adab4b3ca72a7f541da7d0be19
VictorEne-Codes <omalevicene@gmail.com>
2024-07-24 04:53:08 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function to print name
+ * @name: input
+ * @f: input
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
