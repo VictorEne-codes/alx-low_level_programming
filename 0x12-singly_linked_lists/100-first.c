f63b3a068bd27b4c562e246475d39a2e024a1f1a
VictorEne-Codes <omalevicene@gmail.com>
2024-08-02 01:38:47 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void print_before_main(void) __attribute__((constructor));
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
