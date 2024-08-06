f49b5116500296c0c1261195900c4d2ce2f70014
VictorEne-Codes <omalevicene@gmail.com>
2024-08-06 16:19:00 -0700

    task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
+ * reverse_recur - helper function to reverse a listint list
+ * @f: input
+ * @s: input
+ *
+ * Return: listint
+ */
+
+listint_t *reverse_recur(listint_t *f, listint_t *s)
+{
+	listint_t *p, *prev = NULL;
+
+	p = f;
+	while (p->next != s)
+	{
+		prev = p;
+		p = p->next;
+	}
+
+	if (prev != NULL)
+		prev->next = f;
+	s = f->next;
+	f->next = p->next;
+	if (f != p && s != f)
+		s = reverse_recur(s, f);
+	p->next = s;
+	return (p);
+}
+
+/**
+ * reverse_listint - reverses a listint list
  * @head: input
  *
- * Return: listint_t
+ * Return: listint
  */
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *cur = *head;
+	if (head == NULL || *head == NULL)
+		return (NULL);
 
-	while (cur != NULL)
-	{
-		listint_t *next = cur->next;
-		cur->next = prev;
-		prev = cur;
-		cur = next;
-	}
-	*head = prev;
+	*head = reverse_recur(*head, NULL);
 	return (*head);
 }
