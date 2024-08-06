68bcc35bd10405c1dc80a1c4666145a8131fce79
VictorEne-Codes <omalevicene@gmail.com>
2024-08-06 13:23:40 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * print_listint - prints all elements
+ * @h: input
+ *
+ * Return: size_t
+ */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
