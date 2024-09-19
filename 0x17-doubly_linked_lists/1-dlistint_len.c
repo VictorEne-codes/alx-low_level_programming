c87684fab272807ac9396d8573eb537c9f56d48b
VictorEne-Codes <omalevicene@gmail.com>
2024-09-19 13:13:44 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in list
+ * @h: input
+ * 
+ * Return: input
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int counter;
+
+	counter = 0;
+
+	if (h == NULL)
+		return (counter);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
